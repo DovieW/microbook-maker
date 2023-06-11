59264286ef2577df4c6a91e5eaaf59a2a4c55982
Dovie Weinstock <DovieWeinstock.gmail.com>
2023-06-11 13:36:48 -0400

    much better no margins

--- be/index.js
+++ be/index.js
@@ -1,4 +1,3 @@
-// const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
 const puppeteer = require('puppeteer');
@@ -8,7 +7,6 @@ const port = 3001;
 const path = require('path');
 const upload = multer({ dest: 'uploads/' });
 app.use('/generated', express.static(path.join(__dirname, 'generated')));
-// const upload = multer({ storage: multer.memoryStorage() });
 
 app.post('/api/upload', upload.single('file'), (req, res) => {
   const id = Date.now().toString();
@@ -18,28 +16,28 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
     estimatedNumberOfPages = Math.ceil(wordCount / 9700);
   }
 
-  setImmediate(async () => {
-    try {
-      await run(req, id, bookName, fontSize);
-    } catch (error) {
-      console.error(error);
-      const txt = error.toString();
-      const failedPath = path.join(__dirname, 'generated', `FAILED_${id}_${bookName}.txt`);
-      fs.writeFileSync(failedPath, txt);
-    }
-  });
-
   function writeToInProgress(text) {
     console.log(`${text}`);
     const inProgressPath = path.join(__dirname, 'generated', `IN_PROGRESS_${id}_${bookName}.txt`);
     fs.writeFileSync(inProgressPath, text);
   }
 
+  setImmediate(async () => {
+    try {
+      await run(req, id, bookName, fontSize);
+    } catch (error) {
+      console.error(error);
+      writeToInProgress('ERROR: ' + error.toString());
+    }
+  });
+
+
   async function run(req, id, bookName, fontSize) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
     page.on('console', pageIndex => {
-      writeToInProgress(`Creating page ${pageIndex.text()} out of ${estimatedNumberOfPages} (approximately). It should take around ${Number.parseFloat(((estimatedNumberOfPages - Number(pageIndex.text())) * 5) / 60).toFixed(1)} minutes.`);
+      writeToInProgress(`Creating page ${pageIndex.text()} of ${estimatedNumberOfPages}-ish. It should take around ${Number.parseFloat(((estimatedNumberOfPages - Number(pageIndex.text())) * 5) / 60).toFixed(1)}-ish minutes.`);
     });
 
     // await page.setViewport({ width: 816, height: 1056 });
@@ -47,69 +45,71 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
     let text = fs.readFileSync(req.file.path, 'utf8');
     // let text = req.file.buffer.toString('utf8');
     
-
     await page.goto(`file://${__dirname}/page.html`);
     
     await page.addStyleTag({content: `body { font-size: ${fontSize}px; }`});
 
-    writeToInProgress(`Creating ${bookName} (ID: ${id})`);
-    await page.evaluate((text, bookName, fontSize) => {
-      pageIndex = 1;
+    writeToInProgress(`Creating: ${bookName}`);
+
+    await page.evaluate((text, bookName) => {
+      pageIndex = 0;
       frontOrBackText = ':';
-      // document.querySelector('body').style.fontSize = Number(fontSize);
-      document.querySelector('#title').innerHTML = bookName;
-      if (bookName !== '') document.querySelector('#dash').innerHTML = ' - ';
+      // document.querySelector('#title').textContent = bookName;
+      // if (bookName !== '') document.querySelector('#dash').textContent = ' - ';
 
       const words = text.split(' ');
-      let blocks = Array.from(document.querySelectorAll('.grid-item'));
 
       let currentBlockIndex = 0;
-      let currentBlock = blocks[currentBlockIndex];
+      let currentBlock;
 
       function createNewPage() {
-        console.log(pageIndex+1);
-        pageIndex++;
+        console.log(pageIndex);
         const page = document.createElement('div');
         const header = document.createElement('div');
         const pageNumber = document.createElement('h3');
         const frontOrBack = document.createElement('h3');
-        const title = document.createElement('h3');
         const dash = document.createElement('h3');
+        const title = document.createElement('h3');
 
         page.className = 'page';
         header.className = 'header';
         pageNumber.id = 'pageNumber' + pageIndex;
-        frontOrBack.innerHTML = frontOrBackText;
         frontOrBackText = frontOrBackText === '.' ? ':' : '.';
-        title.innerHTML = bookName;
-        dash.innerHTML = ' - ';
+        frontOrBack.textContent = frontOrBackText;
+        title.textContent = bookName;
+        dash.textContent = ' - ';
 
         header.appendChild(pageNumber);
         header.appendChild(frontOrBack);
         if (bookName !== '') header.appendChild(dash);
         header.appendChild(title);
-        page.appendChild(header);
-        const newGrid = document.createElement('div');
-        newGrid.className = 'grid-container';
-        for (let i = 0; i < 16; i++) {
-          const newGridItem = document.createElement('div');
-          newGridItem.className = 'grid-item';
-          newGrid.appendChild(newGridItem);
-        }
-        page.appendChild(newGrid);
-        document.body.appendChild(page);
-        blocks = Array.from(document.querySelectorAll('.grid-item'));
-      }
+        // page.appendChild(header);
 
+        const grid = document.createElement('div');
+        grid.className = 'grid-container';
+        for (let i = 0; i < 16; i++) {
+          const gridItem = document.createElement('div');
+          gridItem.className = 'grid-item';
+          if (i === 0) gridItem.id = 'header' + pageIndex;
+          grid.appendChild(gridItem);
+        }
+
+        page.appendChild(grid);
+        document.body.appendChild(page);
+        document.querySelector('#header' + pageIndex).appendChild(header);
+        blocks = Array.from(document.querySelectorAll('.grid-item'));
+
+        pageIndex++;
+      }
+      createNewPage();
+
+      currentBlock = blocks[currentBlockIndex];
       for (let i = 0; i < words.length; i++) {
-        currentBlock.textContent += ' ' + words[i];
+        currentBlock.innerHTML += ' ' + words[i];
 
         if (currentBlock.scrollHeight > currentBlock.clientHeight) {
           // If the word made the block to overflow, remove it from the block
-          currentBlock.textContent = currentBlock.textContent.slice(
-            0,
-            currentBlock.textContent.length - words[i].length
-          );
+          currentBlock.innerHTML = currentBlock.innerHTML.slice(0, currentBlock.innerHTML.length - words[i].length);
 
           // Move to the next block
           currentBlockIndex += 1;
@@ -118,16 +118,21 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
             currentBlockIndex = blocks.length - 16; // Reset the block index to the first block of the new page
           }
           currentBlock = blocks[currentBlockIndex];
-          currentBlock.textContent += ' ' + words[i]; // Add the word to the new block
+          currentBlock.innerHTML += ' ' + words[i]; // Add the word to the new block
         }
       }
+      
       for (let i = 0; i < pageIndex; i++) {
-        document.querySelector('#pageNumber' + (i+1)).innerHTML = `${i+1}/${pageIndex}`;
+        document.querySelector('#pageNumber' + i).textContent = `${i+1}/${pageIndex}`;
       }
-    }, text, bookName, fontSize);
+    }, text, bookName);
+
     writeToInProgress('Finished creating pages. Writing to file...');
-    // let htmlContent = await page.content();
-    // fs.writeFileSync('output.html', htmlContent);
+
+    let htmlContent = await page.content();
+    const pageHtml = path.join(__dirname, `pageHtml.html`);
+    fs.writeFileSync(pageHtml, htmlContent);
+
     const pdf = await page.pdf({ format: 'Letter' });
     const pdfOutput = path.join(__dirname, 'generated', `${id}_${bookName}.pdf`);
     fs.writeFileSync(pdfOutput, pdf);
@@ -138,37 +143,17 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
   res.json({ message: 'PDF creation started.', id });
 });
 
-app.get('/api/status/', (req, res) => {
-  const { id, bookName } = req.query;
-  const pdfOutput = path.join(__dirname, 'generated', `${id}_${bookName}.pdf`);
-  const failedPdfOutput = path.join(__dirname, 'generated', `FAILED_${id}_${bookName}.pdf`);
-  const inProgressPath = path.join(__dirname, 'generated', `IN_PROGRESS_${id}_${bookName}.txt`);
-  if (fs.existsSync(pdfOutput)) {
-    return res.send('PDF is ready');
-  } else if (fs.existsSync(inProgressPath)) {
-    res.send(fs.readFileSync(inProgressPath, 'utf8'));
-  } else if (fs.existsSync(failedPdfOutput)) {
-    return res.send('PDF generation faild');
-  } else {
-    return res.send('PDF is NOT ready');
-  }
-});
-
 app.get('/api/download/', (req, res) => {
   const { id, bookName } = req.query;
   const pdfOutput = path.join(__dirname, 'generated', `${id}_${bookName}.pdf`);
-  const faildPdfOutput = path.join(__dirname, 'generated', `FAILED_${id}_${bookName}.pdf`);
   const inProgressPath = path.join(__dirname, 'generated', `IN_PROGRESS_${id}_${bookName}.txt`);
+
   if (fs.existsSync(pdfOutput)) {
-    // res.setHeader('Content-Disposition', `inline; filename="${bookName}.pdf"`);
-    // return res.sendFile(pdfOutput);
     res.redirect(`/generated/${id}_${bookName}.pdf`);
   } else if (fs.existsSync(inProgressPath)) {
     res.send(fs.readFileSync(inProgressPath, 'utf8'));
-  } else if (fs.existsSync(faildPdfOutput)) {
-    return res.send('PDF generation failed for some reason 😭');
   } else {
-    return res.send('Not started. It\'s either in the queue or about to start.');
+    return res.send('Not started. It\'s either in the queue, or failed entirely.');
   }
 });
 
