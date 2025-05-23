6a8c06c38b8e4742c3020272d5c80e151e647290
Dovie Weinstock <DovieWeinstock@gmail.com>
2025-05-23 16:24:15 -0400

    add option for branch in auto updating

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 
 cd /app
+
 git fetch
+git checkout ${BRANCH:-master}
 git reset --hard @{upstream}
 
 chmod +x /app/auto_updating_entrypoint.sh
-
 /app/auto_updating_entrypoint.sh
+
