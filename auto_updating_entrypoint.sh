bded6d7d54362d8d88314702e43403a4e467d19f
Dovie Weinstock <DovieWeinstock@gmail.com>
2025-05-25 10:47:37 -0400

    add startup logging

--- auto_updating_entrypoint.sh
+++ auto_updating_entrypoint.sh
@@ -4,12 +4,16 @@ set -euo pipefail
 fc-cache -fv
 
 cd /app/fe
+echo -e "\n\e[34mnpm install Frontend...\e[0m"
 npm install
+echo -e "\n\e[34mBuilding Frontend...\e[0m"
 npm run build
 
 cd /app/be
+echo -e "\n\e[34mnpm install Backend...\e[0m"
 npm install
 
+echo -e "\n\e[34mStarting nginx...\e[0m"
 cp /app/nginx.conf /etc/nginx/nginx.conf
 nginx
 
